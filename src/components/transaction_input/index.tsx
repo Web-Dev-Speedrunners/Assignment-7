3cdbbd93a707af496223e7c84585b8a5afa70265
muskankapoor <muskankapoor016@gmail.com>
2021-04-19 17:38:48 -0400

    update input display

--- src/components/transaction_input/index.tsx
+++ src/components/transaction_input/index.tsx
@@ -2,8 +2,15 @@ import React from "react";
 
 const TransactionInput: React.FC = () => {
   return (
-    
-      <button type = "submit">Submit</button>
+    <form>
+      <div>
+        <h2>Transaction Input Component</h2>
+        <h5>Description</h5>
+        <h5>Amount</h5>
+        <h5>Date</h5>
+        <button type="submit">Submit</button>
+      </div>
+    </form>
   );
 };
 export default TransactionInput;
