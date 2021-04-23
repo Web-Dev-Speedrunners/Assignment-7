bbe69bea2c7b4a49a00ffeec57a5ffb9b4c9bc2d
Xuejin Gao <xuejingao@gmail.com>
2021-04-22 20:43:56 -0400

    Made transaction list recieve prop

--- src/pages/credit/index.tsx
+++ src/pages/credit/index.tsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import TransactionInput from "../../components/transaction_input";
 import TransactionList from "../../components/transaction_list";
+import TransactionContext from "../../contexts/transaction";
 
 const CreditPage: React.FC = () => {
+  const { creditEntries } = useContext(TransactionContext);
+
   return (
     <div>
       <h1>Credit Page</h1>
-      <TransactionList />
+      <TransactionList entries={creditEntries} />
       <TransactionInput />
     </div>
   );
--- src/pages/debit/index.tsx
+++ src/pages/debit/index.tsx
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import TransactionInput from "../../components/transaction_input";
 import TransactionList from "../../components/transaction_list";
 
+import TransactionContext from "../../contexts/transaction";
+
 const DebitPage: React.FC = () => {
+  const { debitEntries } = useContext(TransactionContext);
+
   return (
     <div>
       <h1>Debit Page</h1>
-      <TransactionList />
+      <TransactionList entries={debitEntries} />
       <TransactionInput />
     </div>
   );
