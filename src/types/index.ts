f9b4690df610a0bd9ec31f45a743d09056871446
Blake Vente <venteralph@gmail.com>
2021-04-23 14:28:10 -0400

    fix ts missing `id` field

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,11 +1,13 @@
 export type TransactionEntry = {
-  id: string,
+  id: string;
   description: string;
   amount: number;
   date: Date;
 };
 
 export class TransactionEntryDefault implements TransactionEntry {
+  id: string;
+
   amount: number;
 
   description: string;
@@ -13,6 +15,7 @@ export class TransactionEntryDefault implements TransactionEntry {
   date: Date;
 
   constructor() {
+    this.id = "-1";
     this.amount = 0;
     this.description = "";
     this.date = new Date(0);
